cbc6a8ff8912f2758f1036912e3530691f9dd56b
itsShinomiya <dud112011@hotmail.com>
2024-12-22 16:32:19 -0300

    tela de login

--- ProjetoFinalLP1/Login.Designer.cs
+++ ProjetoFinalLP1/Login.Designer.cs
@@ -28,24 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.userTxt = new System.Windows.Forms.TextBox();
+            this.passTxt = new System.Windows.Forms.TextBox();
+            this.UserLbl = new System.Windows.Forms.Label();
+            this.PassLbl = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // userTxt
+            // 
+            this.userTxt.Location = new System.Drawing.Point(118, 124);
+            this.userTxt.Name = "userTxt";
+            this.userTxt.Size = new System.Drawing.Size(123, 20);
+            this.userTxt.TabIndex = 0;
+            // 
+            // passTxt
+            // 
+            this.passTxt.Location = new System.Drawing.Point(118, 156);
+            this.passTxt.Name = "passTxt";
+            this.passTxt.PasswordChar = '*';
+            this.passTxt.Size = new System.Drawing.Size(123, 20);
+            this.passTxt.TabIndex = 1;
+            // 
+            // UserLbl
+            // 
+            this.UserLbl.AutoSize = true;
+            this.UserLbl.Location = new System.Drawing.Point(113, 108);
+            this.UserLbl.Name = "UserLbl";
+            this.UserLbl.Size = new System.Drawing.Size(43, 13);
+            this.UserLbl.TabIndex = 2;
+            this.UserLbl.Text = "Usuário";
+            // 
+            // PassLbl
+            // 
+            this.PassLbl.AutoSize = true;
+            this.PassLbl.Location = new System.Drawing.Point(115, 144);
+            this.PassLbl.Name = "PassLbl";
+            this.PassLbl.Size = new System.Drawing.Size(38, 13);
+            this.PassLbl.TabIndex = 3;
+            this.PassLbl.Text = "Senha";
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(103, 31);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(157, 39);
+            this.Title.TabIndex = 4;
+            this.Title.Text = "CINEMA";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Location = new System.Drawing.Point(116, 183);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(125, 23);
+            this.btnEntrar.TabIndex = 5;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 244);
+            this.ClientSize = new System.Drawing.Size(363, 244);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.PassLbl);
+            this.Controls.Add(this.UserLbl);
+            this.Controls.Add(this.passTxt);
+            this.Controls.Add(this.userTxt);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox userTxt;
+        private System.Windows.Forms.TextBox passTxt;
+        private System.Windows.Forms.Label UserLbl;
+        private System.Windows.Forms.Label PassLbl;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button btnEntrar;
     }
 }
 
--- ProjetoFinalLP1/Login.cs
+++ ProjetoFinalLP1/Login.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,32 @@ namespace ProjetoFinalLP1
 {
     public partial class Login : Form
     {
+        private MySqlConnection Obj_Conn = new MySqlConnection();
+        private MySqlCommand Obj_CmdSQL = new MySqlCommand();
+        private MySqlDataReader DadosConsulta;
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Obj_Conn.ConnectionString = "Server=sql10.freesqldatabase.com;Database=sql10753597;User=sql10753597;Pwd=xaCHSSHFwZ";
+                Obj_Conn.Open();
+                Obj_CmdSQL.Connection = Obj_Conn;
+            }
+            catch (Exception Erro)
+            {
+                MessageBox.Show("Erro: " + Erro.Message, "Erro de Conexão",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
