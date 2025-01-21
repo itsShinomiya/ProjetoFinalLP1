bdee0a7aea91e28fe195756fa9a931ae83e7ff0c
harryKoala <gabriel.henriquelima23@gmail.com>
2025-01-20 22:10:36 -0300

    UI improvements

--- ProjetoFinalLP1/Login.Designer.cs
+++ ProjetoFinalLP1/Login.Designer.cs
@@ -28,85 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.userTxt = new System.Windows.Forms.TextBox();
             this.passTxt = new System.Windows.Forms.TextBox();
-            this.UserLbl = new System.Windows.Forms.Label();
-            this.PassLbl = new System.Windows.Forms.Label();
-            this.Title = new System.Windows.Forms.Label();
             this.btnEntrar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // userTxt
             // 
-            this.userTxt.Location = new System.Drawing.Point(118, 124);
+            this.userTxt.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.userTxt.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userTxt.Location = new System.Drawing.Point(103, 242);
+            this.userTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.userTxt.Name = "userTxt";
-            this.userTxt.Size = new System.Drawing.Size(123, 20);
+            this.userTxt.Size = new System.Drawing.Size(190, 25);
             this.userTxt.TabIndex = 0;
             // 
             // passTxt
             // 
-            this.passTxt.Location = new System.Drawing.Point(118, 156);
+            this.passTxt.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.passTxt.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passTxt.Location = new System.Drawing.Point(103, 304);
+            this.passTxt.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.passTxt.Name = "passTxt";
             this.passTxt.PasswordChar = '*';
-            this.passTxt.Size = new System.Drawing.Size(123, 20);
+            this.passTxt.Size = new System.Drawing.Size(190, 25);
             this.passTxt.TabIndex = 1;
-            // 
-            // UserLbl
-            // 
-            this.UserLbl.AutoSize = true;
-            this.UserLbl.Location = new System.Drawing.Point(113, 108);
-            this.UserLbl.Name = "UserLbl";
-            this.UserLbl.Size = new System.Drawing.Size(43, 13);
-            this.UserLbl.TabIndex = 2;
-            this.UserLbl.Text = "Usuário";
-            // 
-            // PassLbl
-            // 
-            this.PassLbl.AutoSize = true;
-            this.PassLbl.Location = new System.Drawing.Point(115, 144);
-            this.PassLbl.Name = "PassLbl";
-            this.PassLbl.Size = new System.Drawing.Size(38, 13);
-            this.PassLbl.TabIndex = 3;
-            this.PassLbl.Text = "Senha";
-            // 
-            // Title
-            // 
-            this.Title.AutoSize = true;
-            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Title.Location = new System.Drawing.Point(103, 31);
-            this.Title.Name = "Title";
-            this.Title.Size = new System.Drawing.Size(157, 39);
-            this.Title.TabIndex = 4;
-            this.Title.Text = "CINEMA";
             // 
             // btnEntrar
             // 
-            this.btnEntrar.Location = new System.Drawing.Point(116, 183);
+            this.btnEntrar.BackColor = System.Drawing.Color.PowderBlue;
+            this.btnEntrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEntrar.FlatAppearance.BorderSize = 0;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Segoe UI Semibold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.Color.Black;
+            this.btnEntrar.Location = new System.Drawing.Point(65, 362);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(125, 23);
+            this.btnEntrar.Size = new System.Drawing.Size(228, 37);
             this.btnEntrar.TabIndex = 5;
             this.btnEntrar.Text = "Entrar";
-            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.UseVisualStyleBackColor = false;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(371, 336);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(65, 240);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(31, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(65, 299);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(363, 244);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(368, 430);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.btnEntrar);
-            this.Controls.Add(this.Title);
-            this.Controls.Add(this.PassLbl);
-            this.Controls.Add(this.UserLbl);
             this.Controls.Add(this.passTxt);
             this.Controls.Add(this.userTxt);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,10 +140,10 @@
 
         private System.Windows.Forms.TextBox userTxt;
         private System.Windows.Forms.TextBox passTxt;
-        private System.Windows.Forms.Label UserLbl;
-        private System.Windows.Forms.Label PassLbl;
-        private System.Windows.Forms.Label Title;
         private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- ProjetoFinalLP1/Painel.Designer.cs
+++ ProjetoFinalLP1/Painel.Designer.cs
@@ -37,17 +37,22 @@
             this.gerenciaFuncionario = new System.Windows.Forms.Button();
             this.gerenciaFilme = new System.Windows.Forms.Button();
             this.gerenciaSessão = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStrip1
             // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.arquivoStrip,
             this.ferramentasStrip});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(329, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(607, 31);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -59,13 +64,13 @@
             this.arquivoStrip.Image = ((System.Drawing.Image)(resources.GetObject("arquivoStrip.Image")));
             this.arquivoStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.arquivoStrip.Name = "arquivoStrip";
-            this.arquivoStrip.Size = new System.Drawing.Size(62, 22);
+            this.arquivoStrip.Size = new System.Drawing.Size(75, 28);
             this.arquivoStrip.Text = "&Arquivo";
             // 
             // sairToolStrip
             // 
             this.sairToolStrip.Name = "sairToolStrip";
-            this.sairToolStrip.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStrip.Size = new System.Drawing.Size(117, 26);
             this.sairToolStrip.Text = "&Sair";
             this.sairToolStrip.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
             // 
@@ -75,66 +80,124 @@
             this.ferramentasStrip.Image = ((System.Drawing.Image)(resources.GetObject("ferramentasStrip.Image")));
             this.ferramentasStrip.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.ferramentasStrip.Name = "ferramentasStrip";
-            this.ferramentasStrip.Size = new System.Drawing.Size(85, 22);
+            this.ferramentasStrip.Size = new System.Drawing.Size(104, 24);
             this.ferramentasStrip.Text = "&Ferramentas";
             // 
             // gerenciaSalas
             // 
-            this.gerenciaSalas.Location = new System.Drawing.Point(29, 181);
+            this.gerenciaSalas.BackColor = System.Drawing.Color.PowderBlue;
+            this.gerenciaSalas.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gerenciaSalas.FlatAppearance.BorderSize = 0;
+            this.gerenciaSalas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gerenciaSalas.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gerenciaSalas.Image = ((System.Drawing.Image)(resources.GetObject("gerenciaSalas.Image")));
+            this.gerenciaSalas.Location = new System.Drawing.Point(34, 417);
+            this.gerenciaSalas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gerenciaSalas.Name = "gerenciaSalas";
-            this.gerenciaSalas.Size = new System.Drawing.Size(127, 118);
+            this.gerenciaSalas.Size = new System.Drawing.Size(240, 128);
             this.gerenciaSalas.TabIndex = 1;
             this.gerenciaSalas.Text = "Gerenciar Salas";
-            this.gerenciaSalas.UseVisualStyleBackColor = true;
+            this.gerenciaSalas.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.gerenciaSalas.UseVisualStyleBackColor = false;
             this.gerenciaSalas.Click += new System.EventHandler(this.gerenciaSalas_Click);
             // 
             // gerenciaFuncionario
             // 
-            this.gerenciaFuncionario.Location = new System.Drawing.Point(173, 181);
+            this.gerenciaFuncionario.BackColor = System.Drawing.Color.PowderBlue;
+            this.gerenciaFuncionario.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gerenciaFuncionario.FlatAppearance.BorderSize = 0;
+            this.gerenciaFuncionario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gerenciaFuncionario.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gerenciaFuncionario.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.gerenciaFuncionario.Image = ((System.Drawing.Image)(resources.GetObject("gerenciaFuncionario.Image")));
+            this.gerenciaFuncionario.Location = new System.Drawing.Point(329, 417);
+            this.gerenciaFuncionario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gerenciaFuncionario.Name = "gerenciaFuncionario";
-            this.gerenciaFuncionario.Size = new System.Drawing.Size(127, 118);
+            this.gerenciaFuncionario.Size = new System.Drawing.Size(240, 128);
             this.gerenciaFuncionario.TabIndex = 2;
-            this.gerenciaFuncionario.Text = "Gerenciar Funcionários";
-            this.gerenciaFuncionario.UseVisualStyleBackColor = true;
+            this.gerenciaFuncionario.Text = "Gerenciar   Funcionários";
+            this.gerenciaFuncionario.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.gerenciaFuncionario.UseVisualStyleBackColor = false;
             this.gerenciaFuncionario.Click += new System.EventHandler(this.gerenciaFuncionario_Click);
             // 
             // gerenciaFilme
             // 
-            this.gerenciaFilme.Location = new System.Drawing.Point(29, 39);
+            this.gerenciaFilme.BackColor = System.Drawing.Color.PowderBlue;
+            this.gerenciaFilme.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gerenciaFilme.FlatAppearance.BorderSize = 0;
+            this.gerenciaFilme.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gerenciaFilme.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gerenciaFilme.Image = ((System.Drawing.Image)(resources.GetObject("gerenciaFilme.Image")));
+            this.gerenciaFilme.Location = new System.Drawing.Point(34, 232);
+            this.gerenciaFilme.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gerenciaFilme.Name = "gerenciaFilme";
-            this.gerenciaFilme.Size = new System.Drawing.Size(127, 118);
+            this.gerenciaFilme.Size = new System.Drawing.Size(240, 128);
             this.gerenciaFilme.TabIndex = 5;
             this.gerenciaFilme.Text = "Gerenciar Filmes";
-            this.gerenciaFilme.UseVisualStyleBackColor = true;
+            this.gerenciaFilme.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.gerenciaFilme.UseVisualStyleBackColor = false;
             this.gerenciaFilme.Click += new System.EventHandler(this.gerenciaFilme_Click);
             // 
             // gerenciaSessão
             // 
-            this.gerenciaSessão.Location = new System.Drawing.Point(173, 39);
+            this.gerenciaSessão.BackColor = System.Drawing.Color.PowderBlue;
+            this.gerenciaSessão.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gerenciaSessão.FlatAppearance.BorderSize = 0;
+            this.gerenciaSessão.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gerenciaSessão.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gerenciaSessão.Image = ((System.Drawing.Image)(resources.GetObject("gerenciaSessão.Image")));
+            this.gerenciaSessão.Location = new System.Drawing.Point(329, 232);
+            this.gerenciaSessão.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gerenciaSessão.Name = "gerenciaSessão";
-            this.gerenciaSessão.Size = new System.Drawing.Size(127, 118);
+            this.gerenciaSessão.Size = new System.Drawing.Size(240, 128);
             this.gerenciaSessão.TabIndex = 6;
             this.gerenciaSessão.Text = "Gerenciar Sessões";
-            this.gerenciaSessão.UseVisualStyleBackColor = true;
+            this.gerenciaSessão.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.gerenciaSessão.UseVisualStyleBackColor = false;
             this.gerenciaSessão.Click += new System.EventHandler(this.gerenciaSessão_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(140, -55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 289);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gainsboro;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(607, 184);
+            this.panel1.TabIndex = 8;
             // 
             // Painel
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 338);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(607, 585);
             this.Controls.Add(this.gerenciaFuncionario);
             this.Controls.Add(this.gerenciaSalas);
             this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.gerenciaFilme);
             this.Controls.Add(this.gerenciaSessão);
+            this.Controls.Add(this.gerenciaFilme);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Painel";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Administrador";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,5 +213,7 @@
         private System.Windows.Forms.Button gerenciaFilme;
         private System.Windows.Forms.Button gerenciaSessão;
         private System.Windows.Forms.ToolStripDropDownButton ferramentasStrip;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
